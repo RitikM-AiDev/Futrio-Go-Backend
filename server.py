bf49e8240987641129d8d00e5ec9b73d7537265b
RitikM-AiDev <ritik.aidev@gmail.com>
2026-06-28 14:31:13 +0530

    first push completed

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://futrio-go-backend.onrender.com","https://futrio-go-backend.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
